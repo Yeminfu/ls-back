cc7528d71fd1eb0069e89c64197f4b111eb8fd7b
Yeminfu <yeminfu4319@gmail.com>
2026-05-31 16:07:15 +1000

    сделвл url hello

--- config/settings.py
+++ config/settings.py
@@ -84,6 +84,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': os.getenv('DB_NAME', 'app'),
+        # 'USER': os.getenv('DB_USER', 'app'),
+        # 'PASSWORD': os.getenv('DB_PASSWORD', 'secret'),
+        # 'HOST': os.getenv('DB_HOST', 'db'),
+        # 'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
