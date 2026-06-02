e04fd8756882e8e4450e1260559730a1e3d7927e
Yeminfu <yeminfu4319@gmail.com>
2026-06-02 22:47:32 +1000

    сделал интерфейс для регистрации пользователей

--- api/models.py
+++ api/models.py
@@ -1,12 +1,2 @@
-from django.db import models
+# from django.db import models
 
-
-# Create your models here.
-class Article(models.Model):
-    title = models.TextField(max_length=200)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    disabled = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.title
--- api/views.py
+++ api/views.py
@@ -1,39 +1,6 @@
-from django.contrib.auth.models import User
-from rest_framework import serializers, generics, permissions, viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from api.models import Article
-from api.serializers import ArticleSerializer
-
-
-# Create your views here.
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=8)
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
-
-    def create(self, validated_data):
-        return User.objects.create_user(**validated_data)
+from rest_framework import generics, permissions
+from .serializers import UserRegistrationSerializer
 
 class RegisterView(generics.CreateAPIView):
-    serializer_class = RegisterSerializer
+    serializer_class = UserRegistrationSerializer
     permission_classes = [permissions.AllowAny]
-
-class ArticleViewSet(viewsets.ModelViewSet):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-    @action(detail=True, methods=['post'])
-    def disable(self, request, pk=None):
-        article = self.get_object()
-        article.disabled = True
-        article.save()
-        return Response({'status': 'disabled'})
-
-    @action(detail=True, methods=['post'])
-    def enable(self, request, pk=None):
-        article = self.get_object()
-        article.disabled = False
-        article.save()
-        return Response({'status': 'enabled'})
