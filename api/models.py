4128b3325adc64f7abd9ccb15baffda754b7d8f2
Yeminfu <yeminfu4319@gmail.com>
2026-06-02 21:52:29 +1000

    перенес авторизацию

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Article(models.Model):
+    title = models.TextField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    disabled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
