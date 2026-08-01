0075b49f35b1352062984cfdc4c58126ff59b3a9
root <root@m0roz.nikitta.hlab.kz>
2026-08-01 08:32:51 +0500

    add swager to app

--- src/config/settings.py
+++ src/config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'drf_spectacular',
     'api',
     'mediafiles',
     'volunteers',
@@ -56,6 +57,13 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',  # ← правильный импорт
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'liga-spas API',
+    'VERSION': '1.0.0',
+    'SERVE_PERMISSIONS': ['rest_framework.permissions.AllowAny'],
 }
 
 MIDDLEWARE = [
