83733f50b3467c1e356e8b3647c9ea66c761b55f
Yeminfu <yeminfu4319@gmail.com>
2026-08-04 10:18:53 +0500

    get media

--- src/mediafiles/tests.py
+++ src/mediafiles/tests.py
@@ -1,3 +1,107 @@
-from django.test import TestCase
+import tempfile
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+
+from .models import MediaFile, MediaLink
+
+User = get_user_model()
+
+
+class MediaFileApiTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="tester",
+            email="tester@example.com",
+            password="testpass123",
+        )
+        self.other = User.objects.create_user(
+            username="other",
+            email="other@example.com",
+            password="testpass123",
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def _upload(self):
+        file = SimpleUploadedFile(
+            "photo.jpg",
+            b"file-content",
+            content_type="image/jpeg",
+        )
+        return self.client.post(
+            reverse("media-list"),
+            {"file": file},
+            format="multipart",
+        )
+
+    def test_upload_creates_media_file(self):
+        response = self._upload()
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(MediaFile.objects.filter(id=response.data["id"]).exists())
+
+    def test_retrieve_returns_metadata(self):
+        media = MediaFile.objects.create(
+            file=SimpleUploadedFile(
+                "photo.jpg",
+                b"file-content",
+                content_type="image/jpeg",
+            ),
+            original_name="photo.jpg",
+            size=12,
+        )
+        response = self.client.get(reverse("media-detail", args=[media.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["id"], str(media.id))
+        self.assertEqual(response.data["original_name"], "photo.jpg")
+
+    def test_retrieve_missing_returns_404(self):
+        response = self.client.get(reverse("media-detail", args=["00000000-0000-0000-0000-000000000000"]))
+        self.assertEqual(response.status_code, 404)
+
+    def test_retrieve_requires_auth(self):
+        media = MediaFile.objects.create(
+            file=SimpleUploadedFile(
+                "photo.jpg",
+                b"file-content",
+                content_type="image/jpeg",
+            ),
+            original_name="photo.jpg",
+            size=12,
+        )
+        anonymous = APIClient()
+        response = anonymous.get(reverse("media-detail", args=[media.id]))
+        self.assertEqual(response.status_code, 401)
+
+    def test_list_by_entity(self):
+        media = MediaFile.objects.create(
+            file=SimpleUploadedFile(
+                "photo.jpg",
+                b"file-content",
+                content_type="image/jpeg",
+            ),
+            original_name="photo.jpg",
+            size=12,
+        )
+        MediaLink.objects.create(
+            media_file=media,
+            entity_type="user",
+            entity_id=self.user.id,
+        )
+        response = self.client.get(
+            reverse("media-list"),
+            {"entity_type": "user", "entity_id": self.user.id},
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["id"], str(media.id))
+
+    def test_list_partial_params_returns_400(self):
+        response = self.client.get(
+            reverse("media-list"),
+            {"entity_type": "user"},
+        )
+        self.assertEqual(response.status_code, 400)
--- src/mediafiles/views.py
+++ src/mediafiles/views.py
@@ -1,6 +1,8 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
+from .models import MediaFile
 from .serializers import (
     MediaFileSerializer,
     MediaUploadSerializer,
@@ -8,6 +10,7 @@ from .serializers import (
 
 
 class MediaFileViewSet(viewsets.ViewSet):
+    serializer_class = MediaFileSerializer
 
     def create(self, request):
         serializer = MediaUploadSerializer(data=request.data)
@@ -24,3 +27,40 @@ class MediaFileViewSet(viewsets.ViewSet):
             response_serializer.data,
             status=status.HTTP_201_CREATED,
         )
+
+    def retrieve(self, request, pk=None):
+        media_file = get_object_or_404(MediaFile, pk=pk)
+        serializer = MediaFileSerializer(
+            media_file,
+            context={"request": request},
+        )
+        return Response(serializer.data)
+
+    def list(self, request):
+        entity_type = request.query_params.get("entity_type")
+        entity_id = request.query_params.get("entity_id")
+
+        if bool(entity_type) != bool(entity_id):
+            return Response(
+                {
+                    "detail": (
+                        "entity_type and entity_id must be provided together."
+                    )
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if entity_type and entity_id:
+            queryset = MediaFile.objects.filter(
+                links__entity_type=entity_type,
+                links__entity_id=entity_id,
+            )
+        else:
+            queryset = MediaFile.objects.all()
+
+        serializer = MediaFileSerializer(
+            queryset,
+            many=True,
+            context={"request": request},
+        )
+        return Response(serializer.data)
